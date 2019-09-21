ded697eab4deb644b0a9ae56ebb61028bb1bbaba
ardo1488 <ardo1488@csu.fullerton.edu>
2019-09-21 14:29:41 -0700

    made a random generator for a rectangle with a height of 3 and a width of a max of 40

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,29 @@
 // Random Rectangle
+
+#include<iostream>
+#include<ctime>
+#include<string>
+#include<iomanip>
+#include <random>
+
+
+int main()
+{
+ srand(time(0));
+//Making a variable that generates the random number.
+ int start = rand() % 40 + 1;
+ std::string pound;
+ pound.assign(start, '#');
+
+ //outputting the random number of #
+
+  std::cout << pound << std::endl;
+  std::cout << pound << std::endl;
+  std::cout << pound << std::endl;
+
+
+
+
+
+  return 0;
+}
